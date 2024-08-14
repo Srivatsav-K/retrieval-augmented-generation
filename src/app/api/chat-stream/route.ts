0e60b920c7839667e4011e1cd4cede82225ff100
Srivatsav K <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-08-14 14:13:48 +0530

    update-route-handler-timeout

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -2,6 +2,8 @@ import { ragChat } from "@/lib/ragChat";
 import { aiUseChatAdapter } from "@upstash/rag-chat/nextjs";
 import { NextRequest } from "next/server";
 
+export const maxDuration = 60;
+
 export const POST = async (req: NextRequest) => {
   const { messages, sessionId } = await req.json();
 
