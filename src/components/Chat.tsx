3e08d954b4ce5a86a0b049408231ed5eaf485369
Srivatsav K <104587732+Srivatsav-K@users.noreply.github.com>
2025-05-03 17:05:19 +0530

    feat: add error handling to Chat component with FullpageError display

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,17 +2,31 @@
 
 import { useChat } from "ai/react";
 import { ChatInput } from "./ChatInput";
+import FullpageError from "./FullpageError";
 import Messages from "./Messages";
 
 type ChatProps = {
   sessionId: string;
 };
 const Chat = ({ sessionId }: ChatProps) => {
-  const { messages, handleInputChange, handleSubmit, input, setInput } =
+  const { messages, handleInputChange, handleSubmit, input, setInput, error } =
     useChat({
       api: "/api/chat-stream",
       body: { sessionId },
     });
+
+  if (error) {
+    return (
+      <FullpageError
+        title="Something went wrong"
+        description={"There was an error with the llm provider"}
+        showAction
+        action={() => {
+          window.location.reload();
+        }}
+      />
+    );
+  }
 
   return (
     <div className="flex min-h-full flex-col justify-between gap-2 divide-y">
